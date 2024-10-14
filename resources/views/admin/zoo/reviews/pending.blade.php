a2fb71b9867631cc43a6a5dfa2bf1a5e57de1e5d
Eiyū <isabelle_radermecker@icloud.com>
2024-10-14 19:06:36 +0200

    Update scss reviews pending.blade, Create css/admin/_reviews_card.scss

--- resources/views/admin/zoo/reviews/pending.blade.php
+++ resources/views/admin/zoo/reviews/pending.blade.php
@@ -15,37 +15,39 @@
 
     <div class="container">
         <h2>Gestion des avis</h2>
-        @if( !$reviewsWithPagination['items'] )
-            <p>Il n'y a pas d'avis en attente.</p>
-        @endif
-        @foreach( $reviewsWithPagination['items'] as $review )
-            <div class="card-content-review"
-                 data-id="{{ $review['id'] }}">
-                <h3>{{ $review['author'] }}</h3>
-                <p>{{ $review['content'] }}</p>
-                <div class="btn-check">
-                    <button type="submit"
-                            class="btn-rejected">
-                        <img src="/asset/icons/rejected.svg"
-                             alt="Icône checkbox refuser l'avis">
-                    </button>
-                    <button type="submit"
-                            class="btn-validated">
-                        <img src="/asset/icons/validated.svg"
-                             alt="Icône checkbox valider l'avis">
-                    </button>
+        <div class="card-content">
+            @if( !$reviewsWithPagination['items'] )
+                <p>Il n'y a pas d'avis en attente.</p>
+            @endif
+            @foreach( $reviewsWithPagination['items'] as $review )
+                <div class="card-content-review"
+                     data-id="{{ $review['id'] }}">
+                    <h3>{{ $review['author'].' le '.$review['created_at'] }}</h3>
+                    <p>{{ $review['content'] }}</p>
+                    <div class="btn-check">
+                        <button type="submit"
+                                class="btn-rejected">
+                            <img src="/asset/icons/rejected.svg"
+                                 alt="Icône checkbox refuser l'avis">
+                        </button>
+                        <button type="submit"
+                                class="btn-validated">
+                            <img src="/asset/icons/validated.svg"
+                                 alt="Icône checkbox valider l'avis">
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div class="btn-actions">
-                @if($reviewsWithPagination['prev_page'])
-                    <a href="{{ $reviewsWithPagination['prev_page'] }}">Précédent</a>
-                @endif
+                <div class="btn-actions">
+                    @if($reviewsWithPagination['prev_page'])
+                        <a href="{{ $reviewsWithPagination['prev_page'] }}">Précédent</a>
+                    @endif
 
-                @if($reviewsWithPagination['next_page'])
-                    <a href="{{ $reviewsWithPagination['next_page'] }}">Suivant</a>
-                @endif
-            </div>
-        @endforeach
+                    @if($reviewsWithPagination['next_page'])
+                        <a href="{{ $reviewsWithPagination['next_page'] }}">Suivant</a>
+                    @endif
+                </div>
+            @endforeach
+        </div>
         <div class="send">
             <a href="{{ route('reviews.list') }}"
                class="btn-send">Retour</a>
